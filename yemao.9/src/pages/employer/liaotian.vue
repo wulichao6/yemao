1c411894c332b20af9e07c8ea95090405e234c22
wlc <1782051793@qq.com>
2017-12-14 14:16:00 +0800

    222

---
+++ yemao.9/src/pages/employer/liaotian.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="">
+    <div class="header">
+      <div class="header-left" @click="goback"><img src="../../assets/images/index/back.png"/></div>
+      <span>昵称</span>
+    </div>
+    <!--  信息表-->
+    <div class="content">
+
+    </div>
+    <div class="lt-bottom">
+        <!--<div class="lb-left">-->
+           <!--<input type="text" placeholder="请输入价格" />-->
+        <!--</div>-->
+        <!--<div class="lb-right">-->
+            <!--发送-->
+        <!--</div>-->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+      goback(){
+        this.$router.goBack();
+      },
+      toUrl: function (pagename) {
+        this.$router.push({name: pagename})
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import "../../assets/css/public/public.css";
+  @import "../../assets/css/employer/liaotian.css";
+</style>
