28b57622257b3e4b03aaf84765dfcfdbee9911b3
wlc <1782051793@qq.com>
2017-12-25 17:46:56 +0800

    addnewyemian

---
+++ yemao.9/src/pages/designer/tixianchenggong.vue
@@ -0,0 +1,51 @@
+<template>
+  <div >
+    <!--头部导航-->
+    <div class="header">
+      <span>提现成功</span>
+    </div>
+    <!--发布订单内容-->
+    <div class="content">
+      <div class="cgzt">
+        <div class="tupiao">
+        </div>
+        <div class="wzsm">
+          等待设计师接单
+        </div>
+      </div>
+      <div class="anniu">
+        <div class="fhsy"@click="toUrl('index')">
+          返回首页
+        </div>
+        <div class="ckdd"@click="toUrl('myorderchuli')">
+          查看订单
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+      goback(){
+        this.$router.goBack();
+      },
+      toUrl: function (pagename) {
+        this.$router.push({name: pagename})
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../../assets/css/designer/tixianchenggong.css';
+</style>
---
+++ yemao.9/src/pages/designer/yirenzheng.vue
@@ -0,0 +1,39 @@
+<template>
+  <div >
+    <!--头部导航-->
+    <div class="header">
+      <div class="header-left"@click="goback"><img src="../../../static/images/back.png" /></div>
+      <span>平台认证</span>
+    </div>
+    <div class="content">
+      <div class="cgzt">
+        您已通过平台认证
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    mounted: function () {
+    },
+    methods: {
+      goback(){
+        this.$router.goBack();
+      },
+      toUrl: function (pagename) {
+        this.$router.push({name: pagename})
+      },
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  @import '../../assets/css/designer/pingtairenzheng.css';
+</style>
