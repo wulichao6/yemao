734d161f1f53300288e2b818ba50a4e00b8e333b
cochlea01 <greatmeng01@126.com>
2017-12-27 13:57:56 +0800

    myorderunDeal

--- yemao.9/src/pages/employer/myorderchuli.vue
+++ yemao.9/src/pages/employer/myorderchuli.vue
@@ -13,16 +13,10 @@
         <tab-item class="vux-center" key="2">待交付</tab-item>
         <tab-item class="vux-center" key="3">已完成</tab-item>
       </tab>
-<<<<<<< HEAD
-      <swiper v-model="index" :show-dots="false" class="swps">
-        <swiper-item key="0">
-            <scroller lock-x height="" @on-scroll-bottom="onScrollBottom" ref="scrollerBottom" :scroll-bottom-offst="100">
-=======
       
 <swiper v-model="index" :show-dots="false" :class="'swps'" style="border:2px solid red">
     <swiper-item key="0">
         <scroller lock-x height="" @on-scroll-bottom="onScrollBottom" ref="scrollerBottom" :scroll-bottom-offst="100">
->>>>>>> master
             <div class="ddlist-sjsdai">
                 <div class="ds-top"@click="toUrl('daichulixq')">
                 <div class="ds-img">
@@ -32,31 +26,6 @@
                     <div class="jianjie-top">
                     已有多年建筑设计工作经验，从事建筑方案到效果图，再到建筑施工图都可以完成
                     </div>
-<<<<<<< HEAD
-                    <div class="db-djs">7天后截止报名</div>
-                  </div>
-                </div>
-              </div>
-              <div class="ds-bottom">
-                <div class="db-right">
-                  <div class="db-qxdd">取消订单</div>
-                  <div class="db-sxdd">刷新订单</div>
-                  <div class="db-qrdd">确认订单</div>
-                </div>
-              </div>
-            </div>
-            </scroller>
-        </swiper-item>
-        <swiper-item key="1">
-          <div class="ddlist-sjsdai">
-            <div class="ds-top"@click="toUrl('daizhifu')">
-              <div class="ds-img">
-                <img src="../../../static/images/bj.jpg">
-              </div>
-              <div class="ds-jianjie">
-                <div class="jianjie-top">
-                  已有多年建筑设计工作经验，从事建筑方案到效果图，再到建筑施工图都可以完成
-=======
                     <div class="jianjie-bottom">
                     <div class="db-leixin"><span>建筑设计</span> <span class="yuan">￥</span><span class="yuan">50000</span>
                     </div>
@@ -69,7 +38,6 @@
                     <div class="db-qxdd">取消订单</div>
                     <div class="db-sxdd">刷新订单</div>
                     <div class="db-qrdd">确认订单</div>
->>>>>>> master
                 </div>
                 </div>
             </div>
@@ -222,27 +190,6 @@
   import {Tab, TabItem, Swiper, SwiperItem,LoadMore, Scroller} from 'vux'
   export default {
     components: {
-<<<<<<< HEAD
-      Tab,
-      TabItem,
-      Swiper,
-      SwiperItem,
-      Scroller,
-      LoadMore
-    },
-    data() {
-      return {
-        index: 0,
-        pageNo: 0,
-        pageSize: 10,
-        onFetching:false,
-        showLoading:true,
-        loadtext:"上拉加载",
-        loadmore:"上拉加载",
-        loadrefresh: '正在加载...',
-        loadnomore: '没有更多数据了',
-      }
-=======
         Tab,
         TabItem,
         Swiper,
@@ -263,35 +210,12 @@
             loadrefresh: '正在加载...',
             loadnomore: '没有更多数据了',
         }
->>>>>>> master
     },
     created: function () {
       this.initData();
       this.addData();
     },
     mounted: function () {
-<<<<<<< HEAD
-        this.$nextTick(() => {
-            this.$refs.scrollerEvent.reset({top: 0})
-        })
-        this.$nextTick(() => {
-            this.$refs.scrollerBottom.reset({top: 0})
-        })
-    },
-    methods: {
-        goback() {
-            this.$router.goBack();
-        },
-        toUrl(name) {
-            this.$router.push({name: name});
-        },
-        onScrollBottom () {
-            console.log("onScrollBottom:")
-        },
-        loadMore() {
-            console.log('loadMore');
-        }
-=======
         swiperHeighfix();
     },
     methods: {
@@ -316,7 +240,6 @@
           }, 100)
         }
       },
->>>>>>> master
     }
   }
 </script>
